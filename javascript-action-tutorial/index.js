d850713dfc7f6db6b53a124d6ce117cbe0393115
Andrew Scribner <ca.scribner+1@gmail.com>
2021-01-28 10:37:39 -0500

    Add issue comment context action (#15)
    * Add javascript action
    
    * Add javascript action workflow
    
    * debugging
    
    * debugging
    
    * play with js
    
    * Add basic if statements, activation on issue create
    
    * debug workflow activation

---
+++ javascript-action-tutorial/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  console.log('Testing if this action occurs due to an issue comment')
+  const context = JSON.stringify(github.context, undefined, 2)
+  console.log(`The entire github context: ${context}`);
+  console.log(`Now on to the fun stuff!`);
+  var is_pr_comment = false;
+  var pr_number = null;
+  if (github.context.event_name == "issue_comment" && github.context.event.issue.pull_request != 0 ) {
+    is_pr_comment = true;
+    console.log(`The pr url is: ${github.context.event.issue.pull_request.html_url}`)
+  }
+  // // `who-to-greet` input defined in action metadata file
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+  // console.log(`The event_name: ${github.context.event_name}`)
+  // // think this could be empty too
+  // console.log(`The PR url: ${github.context.event.issue.pull_request.html_url}`)
+} catch (error) {
+  core.setFailed(error.message);
+}
