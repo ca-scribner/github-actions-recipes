24309c7a6d0b6cada5751d87cab7dbfb8e4bf308
Andrew Scribner <ca.scribner+1@gmail.com>
2021-01-28 11:48:39 -0500

    iterating

--- javascript-action-tutorial/index.js
+++ javascript-action-tutorial/index.js
@@ -1,34 +1,57 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+OWNER, COLLABORATOR, NONE
+
 try {
   console.log('Testing if this action occurs due to an issue comment')
-  const context = JSON.stringify(github.context, undefined, 2)
-  console.log(`The entire github context: ${context}`);
-  console.log(`Now on to the fun stuff!`);
+  const allowed_author_associations = JSON.core.getInput('allowed_author_associations')
+
   var is_pr_comment = false;
   var pr_number = null;
+  var is_comment_from_originator = null;
+  var is_allowed_author = null
+  // Debug
+  // const context = JSON.stringify(github.context, undefined, 2)
+  // console.log(`The entire github context: ${context}`);
+  // console.log(`Now on to the fun stuff!`);
+
   if (github.context.eventName == "issue_comment" && github.context.payload.issue.hasOwnProperty("pull_request") ) {
+    console.log(`The comment is from a PR`)
     is_pr_comment = true;
+
     const issue = github.context.payload.issue
-    console.log(`The pr url is: ${issue.pull_request.html_url}`)
+    
+    console.log(`The PR url is: ${issue.pull_request.html_url}`)
     const start_index = issue.pull_request.html_url.lastIndexOf('/') + 1
     pr_number = issue.pull_request.html_url.substring(start_index)
-    console.log(`The pr number is: ${pr_number}`)
+    console.log(`The PR number is: ${pr_number}`)
+
+    // Test if author has an allowed association
+    console.log(`Is the PR is from an allowed author (is ${github.context.payload.comment.author_association} in ${allowed_author_associations}?)`)
+    if (allowed_author_associations.includes(github.context.payload.comment.author_association)) {
+      is_allowed_author = true
+    } else {
+      is_allowed_author = false
+    }
+    console.log(`    ${is_allowed_author}`)
+
+    // Test if author is PR originator
+    console.log(`Is the PR from the originator (is ${github.context.payload.comment.user.login} == ${github.context.payload.issue.user.login}?)`)
+    if (github.context.payload.comment.user.login == github.context.payload.issue.user.login) {
+      is_comment_from_originator = true
+    } else {
+      is_comment_from_originator = false
+    }
+    console.log(`    ${is_comment_from_originator}`)
+
   } else {
-    console.log(`missed the if statement`)
+    console.log(`The comment is not from a PR`)
   }
-  // // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-  // console.log(`The event_name: ${github.context.event_name}`)
-  // // think this could be empty too
-  // console.log(`The PR url: ${github.context.event.issue.pull_request.html_url}`)
+  core.setOutput("is_pr_comment", is_pr_comment)
+  core.setOutput("pr_number", pr_number)
+  core.setOutput("allowed_author", is_allowed_author)
+  core.setOutput("from_pr_originator", is_comment_from_originator)
 } catch (error) {
   core.setFailed(error.message);
 }
