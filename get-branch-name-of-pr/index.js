839d0a51dd8cbe578b7cb120384c970aa2a3bdae
Andrew Scribner <ca.scribner+1@gmail.com>
2021-01-29 11:13:24 -0500

    Iterating

--- get-branch-name-of-pr/index.js
+++ get-branch-name-of-pr/index.js
@@ -5,12 +5,17 @@ const github = require('@actions/github');
 
 async function fetchName(owner, repository, pr_number) {
         try {
+          if (owner === "" || repository === "" || pr_number === "") {
+            console.log("UHHO.  An input is blank")
+            throw new Error('invalid inputs - is an input missing?');
+          }
           const github_url='https://api.github.com/repos'
           const url=`${github_url}/${owner}/${repository}/pulls/${pr_number}`
           const response = await fetch(url, {
                 method: 'GET',
             });
           const pr_data = await response.json();
+          console.log(`pr_data = ${pr_data}`)
           return pr_data.head.ref;
         } catch (error) {
             console.error(error);
@@ -22,7 +27,7 @@ async function main() {
     const owner = core.getInput('owner')
     const repository = core.getInput('repository')
     const pr_number = core.getInput('pr_number')
-    console.log(`Getting branch name of PR ${owner}/${repository}/${pr_number}`)
+    console.log(`Getting branch name of PR owner=${owner}, repository=${repository}, pr_number=${pr_number}`)
 
     const branch_name = await fetchName(owner, repository, pr_number)
     console.log(`Got branch name: ${branch_name}`)
