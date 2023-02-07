d8710e7397a36a8c0332a08b364f97223ad833b5
Andrew Scribner <ca.scribner+1@gmail.com>
2023-02-07 17:12:58 -0500

    edit faily snap

--- .github/workflows/faily_snap.sh
+++ .github/workflows/faily_snap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
-if [[ "$((RANDOM % 5))" -eq 0 ]]; then
+X="$RANDOM"
+echo Random number: $X
+if [[ "$((X % 5))" -eq 0 ]]; then
 	echo snap command failed randomly
 else
 	snap "$@"
